b4da768cd6862ef3051bd37d63fa8f988e369680
Justine Toong <programmerjustine@gmail.com>
2026-06-22 20:58:34 +0800

    feat(mobile): add avatar constants

---
+++ mobile/src/constants/Avatars.js
@@ -0,0 +1,11 @@
+// Using a 1x1 transparent base64 image as placeholder until real assets are provided
+const PLACEHOLDER_IMAGE = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+
+export const AVATARS = [
+  { id: 'explorer_1', uri: PLACEHOLDER_IMAGE },
+  { id: 'explorer_2', uri: PLACEHOLDER_IMAGE },
+  { id: 'mascot_1', uri: PLACEHOLDER_IMAGE },
+  { id: 'student_1', uri: PLACEHOLDER_IMAGE },
+  { id: 'student_2', uri: PLACEHOLDER_IMAGE },
+  { id: 'visitor_1', uri: PLACEHOLDER_IMAGE },
+];
