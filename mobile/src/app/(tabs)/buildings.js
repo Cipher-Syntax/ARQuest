de1651df019492fbaf31499c3babd6b59308cd01
Justine Toong <programmerjustine@gmail.com>
2026-07-02 15:04:52 +0800

    fix(mobile): change status bar to dark to match new white search header

--- mobile/src/app/(tabs)/buildings.js
+++ mobile/src/app/(tabs)/buildings.js
@@ -227,7 +227,7 @@ export default function BuildingsScreen() {
 
     return (
         <View style={styles.container}>
-            <StatusBar style="light" />
+            <StatusBar style="dark" />
             
             {(isUnlockedLoading || isLoadingAll) && !webViewReady && (
                 <View style={styles.loadingOverlay}>
