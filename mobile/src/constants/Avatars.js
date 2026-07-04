e1fc8f16132bc229c4b6f43162bc8308a05188a6
Justine Toong <programmerjustine@gmail.com>
2026-07-04 08:56:19 +0800

    Replaced: avatars

--- mobile/src/constants/Avatars.js
+++ mobile/src/constants/Avatars.js
@@ -1,8 +1,8 @@
 export const AVATARS = [
-  { id: 'explorer_1', source: require('../../assets/avatars/explorer_1.jpg') },
-  { id: 'explorer_2', source: require('../../assets/avatars/explorer_2.jpg') },
-  { id: 'mascot_1', source: require('../../assets/avatars/mascot_1.jpg') },
-  { id: 'student_1', source: require('../../assets/avatars/student_1.jpg') },
-  { id: 'student_2', source: require('../../assets/avatars/student_2.jpg') },
-  { id: 'visitor_1', source: require('../../assets/avatars/visitor_1.jpg') },
+  { id: 'explorer_1', source: require('../../assets/avatars/explorer_1.png') },
+  { id: 'explorer_2', source: require('../../assets/avatars/explorer_2.png') },
+  { id: 'mascot_1', source: require('../../assets/avatars/mascot_1.png') },
+  { id: 'student_1', source: require('../../assets/avatars/student_1.png') },
+  { id: 'student_2', source: require('../../assets/avatars/student_2.png') },
+  { id: 'visitor_1', source: require('../../assets/avatars/visitor_1.png') },
 ];
