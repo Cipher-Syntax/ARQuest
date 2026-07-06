3eeec86935d8b2a6825e0b44b9e6285ddb3c8b83
Justine Toong <programmerjustine@gmail.com>
2026-07-06 18:39:35 +0800

    Fix web avatars not showing up by pointing to real images

--- web/src/utils/avatarUtils.js
+++ web/src/utils/avatarUtils.js
@@ -1,12 +1,10 @@
-const PLACEHOLDER_IMAGE = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
-
 export const AVATARS = [
-  { id: 'explorer_1', uri: PLACEHOLDER_IMAGE },
-  { id: 'explorer_2', uri: PLACEHOLDER_IMAGE },
-  { id: 'mascot_1', uri: PLACEHOLDER_IMAGE },
-  { id: 'student_1', uri: PLACEHOLDER_IMAGE },
-  { id: 'student_2', uri: PLACEHOLDER_IMAGE },
-  { id: 'visitor_1', uri: PLACEHOLDER_IMAGE },
+  { id: 'explorer_1', uri: '/avatars/explorer_1.png' },
+  { id: 'explorer_2', uri: '/avatars/explorer_2.png' },
+  { id: 'mascot_1', uri: '/avatars/mascot_1.png' },
+  { id: 'student_1', uri: '/avatars/student_1.png' },
+  { id: 'student_2', uri: '/avatars/student_2.png' },
+  { id: 'visitor_1', uri: '/avatars/visitor_1.png' },
 ];
 
 export const getAvatarUri = (avatarId) => {
