a457255411eaa28bae930f2062470dada168c9a2
Justine Toong <programmerjustine@gmail.com>
2026-07-22 17:42:54 +0800

    fix(mobile): resolve incorrect relative imports in api and offline queue service

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { authService } from "./authService";
+import { authService } from "./auth/authService";
 
 const EXPO_PUBLIC_API_URL = process.env.EXPO_PUBLIC_API_URL;
 
