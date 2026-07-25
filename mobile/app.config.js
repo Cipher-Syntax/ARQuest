1886fc968ea19aeb92d79360b3fda73c87e86f69
Justine Toong <programmerjustine@gmail.com>
2026-07-25 13:13:19 +0800

    fix(config): change scheme to lowercase arquest to pass EAS validation

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,7 +5,7 @@ export default {
         version: "1.0.0",
         orientation: "portrait",
         icon: "./assets/images/icon.png",
-        scheme: "ARQuest",
+        scheme: "arquest",
         userInterfaceStyle: "automatic",
         
         updates: {
